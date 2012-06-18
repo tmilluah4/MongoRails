533d1b830752ef882991557895f8ec8ef9f965c9
Todd Miller <todd.miller3@yahoo.com>
2012-06-18 16:21:07 -0500

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User
+  include Mongoid::Document
+  field :provider, :type => String
+  field :uid, :type => String
+  field :name, :type => String
+  field :email, :type => String
+  field :password_digest, :type => String
+  has_secure_password
+  attr_accessible   :uid, :name, :email, :password, :password_confirmation
+  validates :name, :presence => true
+
+  #attr_accessible :name, :email, :password, :password_confirmation
+
+
+
+
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:   true,
+            format:     { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoRails::Application.config.secret_token = '9023c251d41b5e95a779fab26088d19f92192142e20a7dac63edabef2b32ce24018b5130a2c59a0fa302ee08d9bde00b89add8dbcece3676b97978f1263306f2'
